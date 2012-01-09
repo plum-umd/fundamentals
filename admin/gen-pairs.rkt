5e2b2a6dc54957cfa0a7b2955932e4b4fb0ee9f6
Sam Tobin-Hochstadt <samth@racket-lang.org>
2012-01-09 03:58:33 +0000

    initial partner assignments

--- admin/gen-pairs.rkt
+++ admin/gen-pairs.rkt
@@ -1,15 +1,15 @@
 #lang racket
 
 
-(define x (sort #:cache-keys? #t #:key (λ _ (random)) (file->lines "student-list") <))
+(define x (shuffle (file->lines "student-list")))
 
 (define one (first x))
 (define others (rest x))
 
-(define pairs (map list (take others 17) 
-                   (drop  others 17)))
+(define pairs (map list (take others 20) 
+                   (drop  others 20)))
 
-(define (get-username s) (second (regexp-match ".*?([a-z0-9]+)$" s)))
+(define (get-username s) (second (regexp-match ".*?([a-z_0-9]+)$" s)))
 
 (define pairs*
   (cons (cons one (first pairs))
@@ -18,24 +18,30 @@
 (define all-users (map (λ (l) (map get-username l)) pairs*))
 
 ;; the first real partnership assignment.  Use to rerun if neccessary.
-(define all-users1
-  '(("mcoppola" "mcarroll" "naraghi")
-    ("lindeman" "robbyk")
-    ("dcorbett" "chiner")
-    ("laplante" "rpless")
-    ("demaior" "kardon")
-    ("mmitlin" "forrest")
-    ("morehd" "gierb")
-    ("hloople" "petey3")
-    ("pfurtado" "dmsilva")
-    ("fungi" "gressen")
-    ("jgramm" "florence")
-    ("hfried7" "ngoldman")
-    ("jccoates" "markefus")
-    ("tswartz" "schwarta")
-    ("wconlon" "aldwin")
-    ("ekelly" "maxgoren")
-    ("fnimick" "wjhsie")))
+(define all-users1 '()
+  #|
+pair 001: tlunter, dcalacci, ian_redpath
+pair 002: gwong, ishaqr
+pair 003: ajhorst, butlerch
+pair 004: stoye, emichel
+pair 005: ckohler, gchan93
+pair 006: ronbrz, cclark
+pair 007: jgoode, cmoss
+pair 008: alec_loudenback, mlhamlin
+pair 009: louella_estillero, rmacnz
+pair 010: william_johnston, matthew_king
+pair 011: chris11, psanshi
+pair 012: ksoldau, erenn16
+pair 013: 11bowerj, bsoohoo
+pair 014: colemanm, rramsey
+pair 015: ajacks, taldrich
+pair 016: asdjkl, gloden
+pair 017: blaisenu, kevrcav
+pair 018: jkantor, pletchst
+pair 019: mullinsk, nathaniel_hahn
+pair 020: mechanik, manning
+|#
+  )
 
 (define (pad i)
   (define s (number->string i))
@@ -45,12 +51,12 @@
 
 (for ([members all-users]
       [i (in-naturals 1)])
-  (printf "[cs2510hspring2011:/pair~a]\n" (pad i))
+  (printf "[cs2510hspring2012:/pair~a]\n" (pad i))
   (for ([m members])
     (printf "~a = rw\n" m))
   (printf "\n"))
 
-(printf "for the blog\n")
+(printf "\nfor the blog\n")
 
 (for ([members all-users]
       [i (in-naturals 1)])
--- web/blog.scrbl
+++ web/blog.scrbl
@@ -7,6 +7,33 @@
 
 @title*{Blog}
 
+@section*{Initial partner assignments}
+
+If your username is not listed, please contact the instructors right away.
+
+@verbatim{
+pair 001: tlunter, dcalacci, ian_redpath
+pair 002: gwong, ishaqr
+pair 003: ajhorst, butlerch
+pair 004: stoye, emichel
+pair 005: ckohler, gchan93
+pair 006: ronbrz, cclark
+pair 007: jgoode, cmoss
+pair 008: alec_loudenback, mlhamlin
+pair 009: louella_estillero, rmacnz
+pair 010: william_johnston, matthew_king
+pair 011: chris11, psanshi
+pair 012: ksoldau, erenn16
+pair 013: 11bowerj, bsoohoo
+pair 014: colemanm, rramsey
+pair 015: ajacks, taldrich
+pair 016: asdjkl, gloden
+pair 017: blaisenu, kevrcav
+pair 018: jkantor, pletchst
+pair 019: mullinsk, nathaniel_hahn
+pair 020: mechanik, manning
+}
+
 @section*{Welcome to CS2510H}
 
 @tt{Sun Jan  8 19:39:16 EST 2012} 
