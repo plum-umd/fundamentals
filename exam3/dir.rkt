e221834665f2e32c42ee1cfa161f60fa7229d5da
David Van Horn <dvanhorn@cs.umd.edu>
2012-02-13 01:08:13 +0000

    Dir source code.

---
+++ exam3/dir.rkt
@@ -0,0 +1,48 @@
+#lang class/1
+
+;; An Elem is one of
+;; - (new file% String Number)
+;; - (new dir% String LoElem)
+;; implements
+;; - list-files : -> LoElem
+
+;; A LoElem is one of
+;; - (new mt%)
+;; - (new cons% Elem LoElem)
+;; implements
+;; - list-files : -> LoElem
+;; - append : LoElem -> LoElem
+
+(define-class dir%
+  (fields name elems)
+  (define (list-files)
+    (this . elems . list-files)))
+
+(define-class file%
+  (fields name size)  
+  (define (list-files)
+    (new cons% this (new mt%))))
+
+(define-class mt%
+  (define (append loe) loe)
+  (define (list-files) this))
+
+(define-class cons%
+  (fields first rest)
+  (define (append loe)
+    (new cons% (this . first) (this . rest . append loe)))
+  (define (list-files)
+    (this . first . list-files . append (this . rest . list-files))))
+    
+(define mt (new mt%))
+(define a (new file% "a.txt" 5))
+(define b (new file% "b.rkt" 10))
+(define c (new file% "c.c" 1000))
+
+(define C/ (new dir% "C" (new cons% c mt)))
+(define B/ (new dir% "B" (new cons% b mt)))
+(define A/ (new dir% "A" (new cons% a (new cons% B/ mt))))
+
+(check-expect 
+ ((new dir% "/" (new cons% A/ (new cons% C/ mt))) . list-files)
+ (new cons% a (new cons% b (new cons% c mt))))
