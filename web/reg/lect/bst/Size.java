f1004a4b37193ab28def20dd6d1825d2f1fbab6d
David Van Horn <dvanhorn@cs.umd.edu>
2012-03-01 13:37:28 +0000

    Small tweak to Size.

--- web/reg/lect/bst/Size.java
+++ web/reg/lect/bst/Size.java
@@ -1,8 +1,6 @@
+// BT visitor that computes the size of the visited bt.
 public class Size<X> implements IVisitorBT<X,Integer> {
-    public Integer visitLeaf() {
-	return 0;
-    }
-
+    public Integer visitLeaf() { return 0; }
     public Integer visitNode(X val, IBT<X> left, IBT<X> right) {
 	return 1 + left.accept(this) + right.accept(this);
     }
