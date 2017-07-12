d10cba0653987ce5106d9f9504cf55876e7bbc66
David Van Horn <dvanhorn@cs.umd.edu>
2017-07-12 17:37:43 -0400

    Web page overhaul.

---
+++ 1/www/defns.rkt
@@ -0,0 +1,9 @@
+#lang racket
+(provide (all-defined-out))
+
+(define m1-date "Oct 2")
+(define m2-date "Nov 6")
+(define final-date "TBD")
+(define year "2017")
+(define courseno "CMSC 131A")
+(define elms-url "https://myelms.umd.edu/courses/1227887")
