60870bc105a3ff98d1f8d4f8924e6076742f5abf
David Van Horn <dvanhorn@cs.umd.edu>
2017-09-01 12:32:00 -0400

    Code, video.

--- 1/www/eclipse.rkt
+++ 1/www/eclipse.rkt
@@ -1,11 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname 8_23_eclipse) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname eclipse) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
 (require 2htdp/universe)
 
-(define sun (circle 100 "solid" "yellow"))
-(define moon(circle 95 "solid" "black"))
+(define sun  (circle 100 "solid" "yellow"))
+(define moon (circle 95 "solid" "black"))
 
 (define (eclipse time)
   (place-image
