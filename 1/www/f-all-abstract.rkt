21ef6385b8b929862e95a3fc7aa359ffaeede9b6
David Van Horn <dvanhorn@cs.umd.edu>
2017-10-13 10:44:28 -0400

    Remove name.

--- 1/www/f-all-abstract.rkt
+++ 1/www/f-all-abstract.rkt
@@ -1,8 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname f-all-abstract) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; Amber Rahman, assign 3
-
 ;; sqr-all : LoN -> LoN
 ;; Square each element of the list
 (define (sqr-all lon)
