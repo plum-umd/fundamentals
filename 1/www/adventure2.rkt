30860ec08d27eacf7b1f63c97cd2a3df0a57abf7
David Van Horn <dvanhorn@cs.umd.edu>
2017-10-27 14:34:12 -0400

    Update code.

--- 1/www/adventure2.rkt
+++ 1/www/adventure2.rkt
@@ -54,12 +54,14 @@
 (require 2htdp/universe)
 
 ;; main : Adventure -> Adventure
+;; Read a choose-your-own-adventure game
 (define (main adv)
   (big-bang adv
             [to-draw adv-draw]
             [on-key adv-key]))
 
 ;; adv-draw : Adventure -> Image
+;; Draw the question or conclusion
 (define (adv-draw adv)
   (overlay (text (cond [(string? adv) adv]
                        [(choice? adv) (choice-q adv)])
@@ -68,6 +70,7 @@
            (empty-scene 800 100)))
 
 ;; adv-key : Adventure KeyEvent -> Adventure
+;; If choice, select yes or no with "y" or "n"
 (define (adv-key adv ke)
   (cond [(string? adv) adv]
         [(choice? adv)
@@ -113,21 +116,52 @@
         [(choice? adv)
          (yesss-ending (choice-yes adv))]))
 
-#|
+
 ;; yesss-story : Adventure -> [Listof String]
 ;; Compute the story of always saying yes
 (check-expect (yesss-story "You lose!") (list "You lose!"))
-(define (yesss-story adv) (list "You lose!"))
+(check-expect (yesss-story (make-choice "Q?" "A1" "A2"))
+              (list "Q?" "A1"))
+(define (yesss-story adv)
+  (cond [(string? adv) (list adv)]
+        [(choice? adv)
+         (cons (choice-q adv)
+               (yesss-story (choice-yes adv)))]))
 
 ;; max-length : Adventure -> Number
 ;; Compute max number of choices that could be made in adv
 (check-expect (max-length "You lose!") 0)
-(define (max-length adv) 0)
+(check-expect (max-length (make-choice "Q?" "A1" "A2")) 1)
+(check-expect (max-length (make-choice "Q?"
+                                       (make-choice "Q2?" "A1" "A2")
+                                       "A3"))
+              2)
+(check-expect (max-length (make-choice "Q?"
+                                       "A3"
+                                       (make-choice "Q2?" "A1" "A2")))
+              2)
+(check-expect (max-length CMSC131A) 4)
+(define (max-length adv)
+  (cond [(string? adv) 0]
+        [(choice? adv)
+         (+ 1 (max (max-length (choice-yes adv))
+                   (max-length (choice-no adv))))]))
 
 ;; all-endings : Adventure -> [Listof String]
 ;; Compute a list of all endings
 (check-expect (all-endings "You lose!") (list "You lose!"))
-(define (all-endings adv) (list "You lose!"))
+(check-expect (all-endings (make-choice "Q?" "A1" "A2"))
+              (list "A1" "A2"))
+(check-expect (all-endings (make-choice "Q?"
+                                        (make-choice "Q2?" "A1" "A2")
+                                        "A3"))
+              (list "A1" "A2" "A3"))
+
+(define (all-endings adv)
+  (cond [(string? adv) (list adv)]
+        [(choice? adv)
+         (append (all-endings (choice-yes adv))
+                 (all-endings (choice-no adv)))]))
 
 ;; all-stories : Adventure -> [Listof [Listof String]]
 ;; Compute all possible stories w/ yes & no answers
@@ -135,16 +169,39 @@
 (check-expect (all-stories (make-choice "Q?" "A" "B"))
               (list (list "Q?" "Yes" "A")
                     (list "Q?" "No" "B")))
-(define (all-stories adv) '())
+(check-expect (all-stories "A") (list (list "A"))) 
+(check-expect (all-stories "B") (list (list "B")))
+(check-expect (all-stories (make-choice "Q?"
+                                        (make-choice "Q2?" "A1" "A2")
+                                        "A3"))
+              (list (list "Q?" "Yes" "Q2?" "Yes" "A1")
+                    (list "Q?" "Yes" "Q2?" "No" "A2")
+                    (list "Q?" "No" "A3")))
+(define (all-stories adv)
+  (cond [(string? adv) (list (list adv))]
+        [(choice? adv)
+         (append
+          (map (λ (s) (cons (choice-q adv) (cons "Yes" s)))
+               (all-stories (choice-yes adv)))
+          (map (λ (s) (cons (choice-q adv) (cons "No" s)))
+               (all-stories (choice-no adv))))]))
 
+#|
+Working through Example 2:
+
+              "Q?"
+              (list (list "A"))
+              (list (list "B"))
+              ;; ==> add question, yes to each yes story
+              ;;     add question, no to each no story
+              (list (list "Q?" "Yes" "A"))
+              (list (list "Q?" "No" "B"))
+              ;; ==> append
+              (list (list "Q?" "Yes" "A")
+                    (list "Q?" "No" "B"))
+|#
 
 ;; A Choices is a [Listof Boolean]
 ;; Interp: a list of Y/N choices to make in a CYOA book
 ;; Choices are listed in the order they are made
 ;; Yes = #true
-
-|#
-
-
-
-      
