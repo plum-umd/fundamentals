0fa84dc6f13caabf57831562f75c01035c98eb4e
David Van Horn <dvanhorn@cs.umd.edu>
2018-11-29 14:35:14 -0500

    Minimal web page for 198Q.

---
+++ 0/www/defns.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(provide (all-defined-out))
+
+(define semester "summer")
+(define year "2019")
+(define courseno "CMSC 198Q")
+
+#|
+(define m1-date "Oct 1")
+(define m2-date "Nov 5")
+(define final-date "Dec 14, 8--10 am")
+(define elms-url "https://myelms.umd.edu/courses/1227887")
+|#
