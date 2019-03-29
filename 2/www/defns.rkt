156b934ee0e581e7bb17db59421abb7ebdc269ac
David Van Horn <dvanhorn@cs.umd.edu>
2019-03-29 08:05:43 -0400

    Final exam date/location.

--- 2/www/defns.rkt
+++ 2/www/defns.rkt
@@ -5,7 +5,7 @@
 ;; FIXME: need a single point of control for the dates.
 (define m1-date "Mar 8")
 (define m2-date "Apr 19")
-(define final-date "TBD")
+(define final-date "May 17 4-6pm CSI 1122")
 (define semester "spring")
 (define year "2019")
 (define courseno "CMSC 132A")
